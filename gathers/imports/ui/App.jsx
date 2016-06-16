194123da061254871e6d5f914258f083930d74d7
NeuTronas - Lukas Ruzauskas <nequadora@gmail.com>
2016-06-17 01:40:32 +0300

    Meteor can now use HTTP functions.

--- gathers/imports/ui/App.jsx
+++ gathers/imports/ui/App.jsx
@@ -9,86 +9,90 @@ import Task from './Task.jsx';
 import AccountsUIWrapper from './AccountsUIWrapper.jsx';
 // App component - represents the whole app
 class App extends Component {
-    constructor(props) {
-        super(props);
+
+    constructor() {
+        super();
         this.state = {
           hideCompleted: false,
         };
     }
-  handleSubmit(event) {
-    event.preventDefault();
- 
-    // Find the text field via the React ref
-    const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
- 
-    Tasks.insert({
-      text,
-      createdAt: new Date(), // current time
-      owner: Meteor.userId(),           // _id of logged in user
-      username: Meteor.user().username,  // username of logged in user
-    });
- 
-    // Clear form
-    ReactDOM.findDOMNode(this.refs.textInput).value = '';
-  }
-
-   toggleHideCompleted() {
-      this.setState({
-        hideCompleted: !this.state.hideCompleted,
-      });
-  }
-
-  renderTasks() {
-      let filteredTasks = this.props.tasks;
-      if (this.state.hideCompleted) {
-         filteredTasks = filteredTasks.filter(task => !task.checked);
+    componentDidUpdate(){
+      if(Meteor.userId()){
+        var steamAPI = ' http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=BC48645A4E6E32C76BB4012F958EF147&steamids=76561197960435530';
+        if(Meteor.userId()){
+          Meteor.http.get(steamAPI, function (err, res) {
+            console.log(res.statusCode, res.data);
+          });
+        }
       }
-      return filteredTasks.map((task) => (
-        <Task key={task._id} task={task} />
-      ));
-  }
+    }
+    handleSubmit(event) {
+      event.preventDefault();
+  
+      // Find the text field via the React ref
+      const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+  
+      Tasks.insert({
+        text,
+        createdAt: new Date(), // current time
+        owner: Meteor.userId(),           // _id of logged in user
+        username: Meteor.user().username,  // username of logged in user
+      });
+  
+      // Clear form
+      ReactDOM.findDOMNode(this.refs.textInput).value = '';
+    }
+    
+    toggleHideCompleted() {
+        this.setState({
+          hideCompleted: !this.state.hideCompleted,
+        });
+    }
 
-  loginWithSteam() {
-      console.log("STEAM");
-      Meteor.loginWithSteam();
-      console.log("Nesuveikia");
-  }
- 
-  render() {
-    return (
-      <div className="container">
-        <header>
-          <h1>Todo List ({this.props.incompleteCount})</h1>
-          <label className="hide-completed">
-            <input
-              type="checkbox"
-              readOnly
-              checked={this.state.hideCompleted}
-              onClick={this.toggleHideCompleted.bind(this)}
-            />
-            Hide Completed Tasks
-          </label>
-
-          <button onClick = {this.loginWithSteam.bind(this)} />
-          <AccountsUIWrapper />
-
-          { this.props.currentUser ?
-            <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
+    renderTasks() {
+        let filteredTasks = this.props.tasks;
+        if (this.state.hideCompleted) {
+          filteredTasks = filteredTasks.filter(task => !task.checked);
+        }
+        return filteredTasks.map((task) => (
+          <Task key={task._id} task={task} />
+        ));
+    }
+  
+    render() {
+      return (
+        <div className="container">
+          <header>
+            <h1>Todo List ({this.props.incompleteCount})</h1>
+            <label className="hide-completed">
               <input
-                type="text"
-                ref="textInput"
-                placeholder="Type to add new tasks"
+                type="checkbox"
+                readOnly
+                checked={this.state.hideCompleted}
+                onClick={this.toggleHideCompleted.bind(this)}
               />
-            </form> : ''
-          }
-        </header>
- 
-        <ul>
-          {this.renderTasks()}
-        </ul>
-      </div>
-    );
-  }
+              Hide Completed Tasks
+            </label>
+
+            <AccountsUIWrapper />
+
+            { this.props.currentUser ?
+              <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
+                <input
+                  type="text"
+                  ref="textInput"
+                  placeholder="Type to add new tasks"
+                />
+              </form> : ''
+            }
+          </header>
+  
+          <ul>
+            {this.renderTasks()}
+          </ul>
+        </div>
+      );
+    }
 }
 
 App.propTypes = {
